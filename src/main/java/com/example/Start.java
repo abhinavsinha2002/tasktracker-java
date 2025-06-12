20fde4db57e57848e0d802d4ba2935dce95833f6
abhinavsinha2002 <abhinavsinha2002@gmail.com>
2025-06-12 13:27:29 +0530

    Create Start.java
    Start file created

---
+++ src/main/java/com/example/Start.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import java.time.LocalDateTime;
+
+public class Start {
+    static int id;
+    static String description="";
+    static String status="";
+    static LocalDateTime createdAt=LocalDateTime.now();
+    static LocalDateTime updatedAt=LocalDateTime.now();
+
+}
